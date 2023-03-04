3a7a4da08282e0160a51f106583282a36ac5876d
Rustam <antinov.rus@mail.ru>
2023-03-05 02:49:45 +0700

    Create full request by retrofit

--- app/src/main/java/com/example/retrofitlessons/MainActivity.kt
+++ app/src/main/java/com/example/retrofitlessons/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.retrofitlessons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.retrofitlessons.databinding.ActivityMainBinding
+import com.example.retrofitlessons.retrofit.ProductApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+        val productApi = retrofit.create(ProductApi::class.java)
+
+        binding.btn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val product = productApi.getProductById(1)
+                runOnUiThread{
+                    binding.title.text = product.title
+                }
+            }
+        }
     }
 }
